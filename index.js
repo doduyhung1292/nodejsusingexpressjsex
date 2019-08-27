2f075fe4ebe4caeb04024e9f0440ffc31ccacbbd
doduyhung1292 <doduyhung1292@gmail.com>
2019-08-27 17:52:10 +0700

    add new file

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const pug = require('pug');
 const express = require('express');
 const app = express();
 const port = 3000;
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+
+const db = low(adapter)
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write()
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.get('/', function (req, res) {
